573dd32c446515071dfed6777a1f0de1a5a38fb2
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-26 19:18:23 +0200

    Rendering results

---
+++ src/components/ResultComp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="results">
+    <p>Your results...</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionLength }}</h1>
+    <router-link to="/">Go back</router-link>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+
+const { numberOfCorrectAnswers, quizQuestionLength } = defineProps([
+  "quizQuestionLength",
+  "numberOfCorrectAnswers",
+]);
+</script>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0px;
+}
+
+p {
+  font-size: 25px;
+}
+
+h1 {
+  font-size: 80px;
+}
+</style>
--- src/views/TopicView.vue
+++ src/views/TopicView.vue
@@ -6,8 +6,13 @@
       :barPercentage="barPercentage" />
     <div>
       <QuestionComp
+        v-if="!showResults"
         :question="quiz.questions[currentQuestionIndex]"
         @selectOption="onOptionSelected" />
+      <ResultComp
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers" />
     </div>
   </div>
 </template>
@@ -15,6 +20,7 @@
 <script setup>
 import QuestionComp from "../components/QuestionComp.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import ResultComp from "../components/ResultComp.vue";
 import { useRoute } from "vue-router";
 import { computed } from "@vue/reactivity";
 import { ref } from "vue";
@@ -25,18 +31,22 @@ const quizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === quizId);
 const currentQuestionIndex = ref(0);
 const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
 
 const onOptionSelected = (isCorrect) => {
   if (isCorrect) {
     numberOfCorrectAnswers.value++;
   }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
   currentQuestionIndex.value++;
 };
-
 const questionStatus = computed(
   () => `${currentQuestionIndex.value}/${quiz.questions.length}`
 );
-
 const barPercentage = computed(
   () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
 );
