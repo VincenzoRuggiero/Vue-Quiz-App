7efb66b43d9b039324d428f245b928660d075fd9
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-26 16:18:01 +0200

    Refactor with components and computed props

--- src/views/TopicView.vue
+++ src/views/TopicView.vue
@@ -1,5 +1,45 @@
 <template>
   <div>
-    <h1>Questions</h1>
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage" />
+    <div>
+      <QuestionComp :question="quiz.questions[currentQuestionIndex]" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import QuestionComp from "../components/QuestionComp.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute, watch, computed } from "vue-router";
+import { ref } from "vue";
+import quizes from "../data/quizes.json";
+
+const route = useRoute();
+
+const quizId = parseInt(route.params.id);
+
+const quiz = quizes.find((q) => q.id === quizId);
+
+const currentQuestionIndex = ref(0);
+
+// const questionStatus = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+//   }
+// );
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`;
+});
+
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+</script>
+
+<style scoped></style>
